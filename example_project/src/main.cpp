b0c8860a777832ee2b6794ebb824a296dd9faf01
CodingWithLaura <laura.ganss@web.de>
2023-04-03 19:24:35 +0200

    Save user input in variable and print it

--- example_project/src/main.cpp
+++ example_project/src/main.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
-  cout << "Hello, World!\n";
+  cout << "Please enter your first name:\n";
+  string first_name;
+  cin >> first_name;
+  cout << "Hello, " << first_name  << "!\n";
   return 0;
 }
