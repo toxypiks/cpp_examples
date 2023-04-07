04a68a550320a71efb2ea7e8979471040eda36dc
CodingWithLaura <laura.ganss@web.de>
2023-04-07 17:33:44 +0200

    Exploring a save way to get the ascii value of a char

--- chapter_three/src/main.cpp
+++ chapter_three/src/main.cpp
@@ -2,13 +2,14 @@
 
 int main()
 {
-  int number_of_words = 0;
-  string previous = " ";
-  string current;
-  while(cin >>current) {
-    number_of_words++;
-    if(previous == current)
-      cout << "word_number: " << number_of_words << " repeated word: " << current << "\n";
-    previous = current;
-  }
+  //save way to get the value of a character with no information loss
+  char c = 'x';
+  int i1 = c;
+  int i2 = 'x';
+  cout << "value c: " << c << " value i1: " << i1 << " value i2: " <<  i2 << "\n";
+
+  //you can copy the resutling int back into a char and get the original value
+  char c2 = i1;
+  cout << "value of c2: " <<  c2 << "\n";
 }
+ 
