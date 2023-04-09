7c8f16b18fb7567684e4b9905f09ed0a85efe720
CodingWithLaura <laura.ganss@web.de>
2023-04-09 18:40:40 +0200

    Practising multiple if statements with a dollar calculator

--- chapter_four/src/main.cpp
+++ chapter_four/src/main.cpp
@@ -1,11 +1,17 @@
 #include "std_lib_facilities.h"
 
 int main() {
-  double a = 5;
-  double b = 2;
-  double c = 5/2; //Integer division leads to value 2
-  double d = 5.0/2; //value = 2.5
-  double e = 5/2.0; //value = 2.5
-  cout << a/b << " " << c << " " << d << " " << e << "\n";
-  //lesson learned: if you want a double as a result of a division you need to specify at least one value as double
+  cout << "Please enter a number followed by a unit which you want to transalte into dollars ('y' for yen, 'k' for kroner and 'p' for pounds)) \n";
+  double amount = 0;
+  char unit = ' ';
+  cin >> amount >> unit;
+
+  if(unit == 'y')
+    cout << amount << " dollars are " << amount * 132.14 << " yen\n";
+  else if (unit == 'k')
+    cout << amount << " dollars are " << amount * 10.50 << " kroner\n";
+  else if (unit == 'p')
+    cout << amount << " dollars are " << amount * 0.81 << " pounds\n";
+  else
+    cout << "Sorry, I don't know a unit called '" << unit << "'\n";
 }
