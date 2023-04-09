2c5f1801cebe881ef02c8419f25d10cfd0bc2e93
CodingWithLaura <laura.ganss@web.de>
2023-04-09 19:06:13 +0200

    Practise while loop with output exercise

--- chapter_four/src/main.cpp
+++ chapter_four/src/main.cpp
@@ -1,23 +1,11 @@
 #include "std_lib_facilities.h"
 
 int main() {
-  cout << "Please enter a number followed by a unit which you want to transalte into dollars ('y' for yen, 'k' for kroner and 'p' for pounds)) \n";
-  double amount = 0;
-  char unit = ' ';
-  cin >> amount >> unit;
-
-  switch(unit) {
-    case 'y':
-      cout << amount << " dollars are " << amount * 132.14 << " yen\n";
-      break;
-    case 'k':
-      cout << amount << " dollars are " << amount * 10.50 << " kroner\n";
-      break;
-    case 'p':
-      cout << amount << " dollars are " << amount * 0.81 << " pounds\n";
-      break;
-    default:
-      cout << "Sorry, I don't know a unit called '" << unit << "'\n";
-      break;
+  int i = 0;
+  char j = 'a';
+  int char_val = j;
+  while(i<26) {
+    cout << char(j+i) << "\t" << j+i  << "\n";
+    ++i;
   }
 }
