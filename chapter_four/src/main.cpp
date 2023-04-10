7deef094f55485c1b616d205b8cad19e00ae82cf
CodingWithLaura <laura.ganss@web.de>
2023-04-10 11:12:36 +0200

    Drill 1 till 5

--- chapter_four/src/main.cpp
+++ chapter_four/src/main.cpp
@@ -1,15 +1,24 @@
 #include "std_lib_facilities.h"
 
 int main() {
-  vector<string> words;
-  for(string word; cin>>word;)
-    words.push_back(word);
-  cout << "Number of words: " << words.size() << "\n";
-
-  string disliked = "lemon";
-  for(int i = 0; i < words.size(); i++) 
-    if(words[i] == disliked)
-      cout << "BLEEP\n";
-    else
-      cout << words[i] << "\n";
+  //drill forth chapter
+  int x = 0;
+  double one = 0;
+  double two = 0;
+  vector<double> input_numbers;
+  while(x < 100) {
+    cin >> one >> two;
+    input_numbers.push_back(one);
+    input_numbers.push_back(two);
+    sort(input_numbers);
+    if(input_numbers[0] != input_numbers[1] && (input_numbers[1] - input_numbers[0]) < 1.0/100)
+      cout << "The smaller value is: " << input_numbers[0] << " The bigger value is: " << input_numbers[1]
+           << "\n" << "The numbers are almost equal\n";
+    else if(input_numbers[0] != input_numbers[1]&& (input_numbers[1] - input_numbers[0]) > 1.0/100)
+      cout << "The smaller value is: " << input_numbers[0] << " The bigger value is: " << input_numbers[1];
+    else if(input_numbers[0] == input_numbers[1])
+      cout << "The numbers are equal\n";
+    input_numbers = {};
+    x++;
+  }
 }
