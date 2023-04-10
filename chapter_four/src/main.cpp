77018797bb665bf028cefab1dc1d72e8c7f01937
CodingWithLaura <laura.ganss@web.de>
2023-04-10 08:24:39 +0200

    Write out also uppercase letters

--- chapter_four/src/main.cpp
+++ chapter_four/src/main.cpp
@@ -1,9 +1,9 @@
 #include "std_lib_facilities.h"
 
 int main() {
-  char j = 'a';
+  char j = 'A';
   int char_val = j;
-  for (int i = 0; i<26; i++) {
+  for (int i = 0; i<58; i++) {
     cout << char(j+i) << "\t" << j+i  << "\n";
   }
 }
