367a7b23553e69bad3e0278f0ad6774ffce32be5
CodingWithLaura <laura.ganss@web.de>
2023-04-10 08:19:51 +0200

    Use a for-loop instead of a while loop

--- chapter_four/src/main.cpp
+++ chapter_four/src/main.cpp
@@ -1,11 +1,9 @@
 #include "std_lib_facilities.h"
 
 int main() {
-  int i = 0;
   char j = 'a';
   int char_val = j;
-  while(i<26) {
+  for (int i = 0; i<26; i++) {
     cout << char(j+i) << "\t" << j+i  << "\n";
-    ++i;
   }
 }
