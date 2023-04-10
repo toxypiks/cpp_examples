b15273c88095f81577d36ea6cb518b9da0ff0f9c
CodingWithLaura <laura.ganss@web.de>
2023-04-10 09:47:10 +0200

    Use vector to collect data from user prompt and filter output

--- chapter_four/src/main.cpp
+++ chapter_four/src/main.cpp
@@ -1,14 +1,15 @@
 #include "std_lib_facilities.h"
 
-void my_square(double x) {
-  double square_value = 0;
-  for(int i = 0; i < x; ++i){
-    square_value += x;
-  }
-  cout << square_value << "\n";
-}
-
 int main() {
-  double my_value = 4;
-  my_square(my_value);
+  vector<string> words;
+  for(string word; cin>>word;)
+    words.push_back(word);
+  cout << "Number of words: " << words.size() << "\n";
+
+  string disliked = "lemon";
+  for(int i = 0; i < words.size(); i++) 
+    if(words[i] == disliked)
+      cout << "BLEEP\n";
+    else
+      cout << words[i] << "\n";
 }
