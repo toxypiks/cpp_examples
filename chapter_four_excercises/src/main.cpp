1c3f6b8519f5711603a5ce9d4bec4d0a06a74ee4
CodingWithLaura <laura.ganss@web.de>
2023-04-13 14:19:21 +0200

    Program a calculator

--- chapter_four_excercises/src/main.cpp
+++ chapter_four_excercises/src/main.cpp
@@ -1,47 +1,27 @@
 #include "std_lib_facilities.h"
 
-int my_binary_search(vector<int> numbers) {
-
-  int low_pointer {numbers[0]};
-  int high_pointer {numbers[numbers.size()-1]};
-  int middle_element {(low_pointer + (high_pointer - low_pointer))/2};
-  char answer {' '};
-
-  while(low_pointer <= high_pointer){
-    int range_left = middle_element - low_pointer;
-    int range_right = high_pointer - middle_element;
-
-    if ((range_left == 0) && (range_right == 1))
-      return middle_element + 1;
-    if ((range_left == 1) && (range_right == 0))
-      return middle_element - 1;
-    cout << "Is the number higher then: " << middle_element << " ?(y/n)\n";
-    cin >> answer;
-    
-    if (answer == 'y'){ 
-      low_pointer = middle_element+1;
-      middle_element = ((high_pointer - low_pointer)/2) + low_pointer;
-    }
-    else {
-      high_pointer = middle_element-1;
-      middle_element = high_pointer/2; 
-    }
-    if(low_pointer == high_pointer)
-      return  low_pointer;
-  }
+double my_calculator(double first_num, double second_num, char operation){
+  if (operation == '+')
+    return first_num + second_num;
+  else if (operation == '/')
+    return first_num / second_num;
+  else if (operation == '*')
+    return first_num * second_num;
+  else if (operation == '-')
+    return first_num - second_num;
   return -1;
 }
 
 
-int main() {
-  int number {0};
-  cout<<"Guess a number between 1 and 100: \n";
+int main(){
 
-  vector<int> numbers(100);
-  for (int i = 0; i < numbers.size(); i++) {
-    numbers[i] = i+1;
-  }
-  int result = my_binary_search(numbers);
-  cout<<result;
-  return -1;
-} 
+  double first_num {0};
+  double second_num {0};
+  char operation {' '};
+
+  cout << "Please type in 2 numbers and the operation you wanna execute ('+','-','/','*')\n";
+  cin >> first_num >> second_num >> operation;
+  double result = my_calculator(first_num, second_num, operation);
+  cout << first_num << " " << operation << " " << second_num << " equals " << result << "\n"; 
+
+}
