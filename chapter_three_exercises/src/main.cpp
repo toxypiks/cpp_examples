ea09e23d1415ad52078e45466cf4341a330ab2a6
CodingWithLaura <laura.ganss@web.de>
2023-04-17 12:11:56 +0200

    Write a program that converts spelled-out numbers to integers

--- chapter_three_exercises/src/main.cpp
+++ chapter_three_exercises/src/main.cpp
@@ -1,15 +1,24 @@
 #include "std_lib_facilities.h"
 
-void odd_or_even(int value){
-  if (value % 2 == 0)
-    cout << "The value " << value << " is even\n";
+void words_to_digits(string word){
+  if (word == "zero")
+    cout << 0 << "\n";
+  else if (word == "one")
+    cout << 1 << "\n";
+  else if (word == "two")
+    cout << 2 << "\n";
+  else if (word == "three")
+    cout << 3 << "\n";
+  else if (word == "four")
+    cout << 4 << "\n";
   else
-    cout << "The value " << value << " is odd\n";
+    cout << "Sorry, I dont know the word: " << word << "\n";
+    
 }
 
 int main(){
-  cout << "Type in a number to proof if its even or odd: \n";
-  int value {0};
-  cin >> value;
-  odd_or_even(value);
+  cout << "Write out a number between zero and four: \n";
+  string user_input {" "};
+  cin >> user_input;
+  words_to_digits(user_input);
 }
