2e25d737f0ddfe50f4b9cbc498e6a3ce29857fce
CodingWithLaura <laura.ganss@web.de>
2023-04-17 11:37:50 +0200

    Write a program to analyze two values

--- chapter_three_exercises/src/main.cpp
+++ chapter_three_exercises/src/main.cpp
@@ -1,14 +1,25 @@
 #include "std_lib_facilities.h"
 
-double miles_to_km(double miles){
-  return miles * 1.609;
+void analyze_values(int val1, int val2){
+  if(val1 < val2)
+    cout << val2 << " is bigger then " << val1 << "\n";
+  else
+    cout << val1 << " is bigger then " << val2 << "\n";
+  int sum = val1 + val2;
+  cout << "The sum of " << val1 << " and " << val2 << " is: " << sum << "\n";
+  int difference = val1 - val2;
+  cout << "The difference between " << val1 << " and " << val2 << " is: " << difference << "\n";
+  int product = val1 * val2;
+  cout << "The product of " << val1 << " and " << val2 << " is: " << product << "\n";
+  int ratio = val1 / val2;
+  cout << "The ratio of " << val1 << " and " << val2 << " is: " << ratio << "\n";
 }
 
-int main(){
 
-  cout << "Type in miles to convert to kilometers: \n";
-  double miles {0.0};
-  cin >> miles;
-  double kilometers = miles_to_km(miles);
-  cout << miles << " miles are " << kilometers << " kilometers\n";
+int main(){
+  cout << "Type in two numbers: \n";
+  int user_val1 {0};
+  int user_val2 {0};
+  cin >> user_val1 >> user_val2;
+  analyze_values(user_val1, user_val2);
 }
