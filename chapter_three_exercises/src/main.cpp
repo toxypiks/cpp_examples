1ab47ae8d90d922081103597591be40e858e179e
CodingWithLaura <laura.ganss@web.de>
2023-04-17 11:50:22 +0200

    Write program to format output

--- chapter_three_exercises/src/main.cpp
+++ chapter_three_exercises/src/main.cpp
@@ -1,25 +1,19 @@
 #include "std_lib_facilities.h"
 
-void analyze_values(double val1, double val2){
-  if(val1 < val2)
-    cout << val2 << " is bigger then " << val1 << "\n";
-  else
-    cout << val1 << " is bigger then " << val2 << "\n";
-  double sum = val1 + val2;
-  cout << "The sum of " << val1 << " and " << val2 << " is: " << sum << "\n";
-  double difference = val1 - val2;
-  cout << "The difference between " << val1 << " and " << val2 << " is: " << difference << "\n";
-  double product = val1 * val2;
-  cout << "The product of " << val1 << " and " << val2 << " is: " << product << "\n";
-  double ratio = val1 / val2;
-  cout << "The ratio of " << val1 << " and " << val2 << " is: " << ratio << "\n";
+void sort_value_output(int val1, int val2, int val3){
+  vector<int> values;
+  values.push_back(val1);
+  values.push_back(val2);
+  values.push_back(val3);
+  sort(values);
+  cout << values[0] << ", " <<  values[1] << ", " << values[2] << "\n";
 }
 
-
 int main(){
-  cout << "Type in two floating-point numbers: \n";
-  double user_val1 {0};
-  double user_val2 {0};
-  cin >> user_val1 >> user_val2;
-  analyze_values(user_val1, user_val2);
+  cout << "Enter three values: \n";
+  int value1 {0};
+  int value2 {0};
+  int value3 {0};
+  cin >> value1 >> value2 >> value3;
+  sort_value_output(value1, value2, value3);
 }
