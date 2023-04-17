32fa3beb2f21808bc54afbd938529723b65c8522
CodingWithLaura <laura.ganss@web.de>
2023-04-17 11:55:59 +0200

    Rewrite program to accept strings instead of ints

--- chapter_three_exercises/src/main.cpp
+++ chapter_three_exercises/src/main.cpp
@@ -1,19 +1,19 @@
 #include "std_lib_facilities.h"
 
-void sort_value_output(int val1, int val2, int val3){
-  vector<int> values;
-  values.push_back(val1);
-  values.push_back(val2);
-  values.push_back(val3);
-  sort(values);
-  cout << values[0] << ", " <<  values[1] << ", " << values[2] << "\n";
+void sort_value_output(string word1, string word2, string word3){
+  vector<string> words;
+  words.push_back(word1);
+  words.push_back(word2);
+  words.push_back(word3);
+  sort(words);
+  cout << words[0] << ", " <<  words[1] << ", " << words[2] << "\n";
 }
 
 int main(){
-  cout << "Enter three values: \n";
-  int value1 {0};
-  int value2 {0};
-  int value3 {0};
-  cin >> value1 >> value2 >> value3;
-  sort_value_output(value1, value2, value3);
+  cout << "Enter three words: \n";
+  string word_1 {0};
+  string word_2 {0};
+  string word_3 {0};
+  cin >> word_1 >> word_2 >> word_3;
+  sort_value_output(word_1, word_2, word_3);
 }
