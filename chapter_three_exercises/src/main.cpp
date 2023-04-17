a2d4db61321c1b63f03ca44ddc115c0808ffeb23
CodingWithLaura <laura.ganss@web.de>
2023-04-17 11:40:15 +0200

    Rewrite program to accept floating point numbers to see different results

--- chapter_three_exercises/src/main.cpp
+++ chapter_three_exercises/src/main.cpp
@@ -1,25 +1,25 @@
 #include "std_lib_facilities.h"
 
-void analyze_values(int val1, int val2){
+void analyze_values(double val1, double val2){
   if(val1 < val2)
     cout << val2 << " is bigger then " << val1 << "\n";
   else
     cout << val1 << " is bigger then " << val2 << "\n";
-  int sum = val1 + val2;
+  double sum = val1 + val2;
   cout << "The sum of " << val1 << " and " << val2 << " is: " << sum << "\n";
-  int difference = val1 - val2;
+  double difference = val1 - val2;
   cout << "The difference between " << val1 << " and " << val2 << " is: " << difference << "\n";
-  int product = val1 * val2;
+  double product = val1 * val2;
   cout << "The product of " << val1 << " and " << val2 << " is: " << product << "\n";
-  int ratio = val1 / val2;
+  double ratio = val1 / val2;
   cout << "The ratio of " << val1 << " and " << val2 << " is: " << ratio << "\n";
 }
 
 
 int main(){
-  cout << "Type in two numbers: \n";
-  int user_val1 {0};
-  int user_val2 {0};
+  cout << "Type in two floating-point numbers: \n";
+  double user_val1 {0};
+  double user_val2 {0};
   cin >> user_val1 >> user_val2;
   analyze_values(user_val1, user_val2);
 }
