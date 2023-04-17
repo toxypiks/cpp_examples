4bf2a70b242de1b51a03d9b4e906171190bbef39
CodingWithLaura <laura.ganss@web.de>
2023-04-17 12:02:05 +0200

    Write a program which calculates if a value is even or odd

--- chapter_three_exercises/src/main.cpp
+++ chapter_three_exercises/src/main.cpp
@@ -1,19 +1,15 @@
 #include "std_lib_facilities.h"
 
-void sort_value_output(string word1, string word2, string word3){
-  vector<string> words;
-  words.push_back(word1);
-  words.push_back(word2);
-  words.push_back(word3);
-  sort(words);
-  cout << words[0] << ", " <<  words[1] << ", " << words[2] << "\n";
+void odd_or_even(int value){
+  if (value % 2 == 0)
+    cout << "The value " << value << " is even\n";
+  else
+    cout << "The value " << value << " is odd\n";
 }
 
 int main(){
-  cout << "Enter three words: \n";
-  string word_1 {0};
-  string word_2 {0};
-  string word_3 {0};
-  cin >> word_1 >> word_2 >> word_3;
-  sort_value_output(word_1, word_2, word_3);
+  cout << "Type in a number to proof if its even or odd: \n";
+  int value {0};
+  cin >> value;
+  odd_or_even(value);
 }
