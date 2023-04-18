ba3ae593e453a564d0b511e49129da30b77d46f9
CodingWithLaura <laura.ganss@web.de>
2023-04-18 11:21:12 +0200

    Write program that counts money from user input and convert it to cents

--- chapter_three_exercises/src/main.cpp
+++ chapter_three_exercises/src/main.cpp
@@ -1,24 +1,43 @@
 #include "std_lib_facilities.h"
 
-double calculation(string operation, double val1, double val2){
-  if(operation == "+" || operation == "plus")
-    return val1 + val2;
-  else if(operation == "-" || operation == "minus")
-    return val1 - val2;
-  else if(operation == "*" || operation == "mul")
-    return val1 * val2;
-  else if(operation == "/" || operation == "div")
-    return val1 / val2;
-  return -1;
+void count_user_money(){
+  double pennies {0};
+  double nickels {0};
+  double dimes {0};
+  double quarters {0};
+  double half_dollars {0};
+  double dollars {0};
+  double value_in_pennies {0};
+  cout << "How many pennies you have?\n";
+  cin >> pennies;
+  cout << "How many nickels you have?\n";
+  cin >> nickels;
+  cout << "How many dimes you have?\n";
+  cin >> dimes;
+  cout << "How many quarters you have?\n";
+  cin >> quarters;
+  cout << "How many half_dollars you have?\n";
+  cin >> half_dollars;
+  cout << "How many dollars you have?\n";
+  cin >> dollars;
+  
+  value_in_pennies += pennies;
+  value_in_pennies += nickels * 5;
+  value_in_pennies += dimes * 10;
+  value_in_pennies += quarters * 25;
+  value_in_pennies += half_dollars * 50;
+  value_in_pennies += dollars * 100;
+
+  cout << "You have " << pennies << " pennies\n";
+  cout << "You have " << nickels << " nickels\n";
+  cout << "You have " << dimes << " dimes\n";
+  cout << "You have " << quarters << " quarters\n";
+  cout << "You have " << half_dollars << " half_dollars\n";
+  cout << "You have " << dollars << " dollars\n";
+  cout << "The value of all your coins is " << value_in_pennies << " cents\n";
 }
 
-int main(){
 
-  cout << "Type in an operation (+/plus,-/minus,*/mul or //div) followed by two numbers: \n";
-  string operation {' '};
-  double value1 {0};
-  double value2 {0};
-  cin >> operation >> value1 >> value2;
-  double result = calculation(operation,value1,value2);
-  cout << result << "\n";
+int main(){
+  count_user_money();
 }
