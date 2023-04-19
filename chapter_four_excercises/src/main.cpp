4538a6dc7da45397b01465ff82da272abbfad59a
CodingWithLaura <laura.ganss@web.de>
2023-04-19 17:18:16 +0200

    Create a paper, rock, scissors game using nested switch statements

--- chapter_four_excercises/src/main.cpp
+++ chapter_four_excercises/src/main.cpp
@@ -1,17 +1,64 @@
 #include "std_lib_facilities.h"
 
 int main(){
+  cout << "Lets play paper, rock, scissors. Type in p, r or s: \n";
+  char player_input {' '};
+  cin >> player_input;
+  vector<char> computer_input;
+  computer_input.push_back('p');
+  computer_input.push_back('r');
+  computer_input.push_back('s');
 
-  int number_square {0};
-  double grains_current_square {0};
-  double grains_previous_square {1};
-  
-  //calc for all squares on a chess board (204)
-  //integer can only represent number after 29 squares (1073741824 grains of rice)
-  while(number_square <= 204){
-    grains_current_square = 2* grains_previous_square;
-    grains_previous_square = grains_current_square;
-    number_square++;
+  srand((unsigned) time(NULL));
+  int random = rand() % 3;
+  char computer_input_rand = computer_input[random];
+
+  if(player_input == 'p'){
+
+    switch(computer_input_rand){
+      case'p':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". Nobody wins.\n";
+      break;
+
+      case'r':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". You win.\n";
+      break;
+
+      case's':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". The computer wins.\n";
+      break;
+    }
   }
-  cout << "You need " << number_square << " squares, to end up with " << grains_current_square << " grains of rice.\n";
+  else if(player_input == 'r'){
+    
+    switch(computer_input_rand){
+      case'p':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". The computer wins.\n";
+      break;
+
+      case'r':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". Nobody wins.\n";
+      break;
+
+      case's':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". You win.\n";
+      break;
+    }
+  }
+  else if(player_input == 's'){
+
+    switch(computer_input_rand){
+      case'p':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". You win.\n";
+      break;
+
+      case'r':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". The computer wins.\n";
+      break;
+
+      case's':
+      cout << "You chose " << player_input << ", the computer chose " << computer_input_rand << ". Nobody wins.\n";
+      break;
+    }
+  }
 }
