d308e67a96316dfb082f1c459725be82bb1477c2
CodingWithLaura <laura.ganss@web.de>
2023-04-19 21:04:35 +0200

    Modify function to accept a max value for size of vector to search for prime numbers

--- chapter_four_excercises/src/main.cpp
+++ chapter_four_excercises/src/main.cpp
@@ -1,25 +1,29 @@
 #include "std_lib_facilities.h"
 
-vector<int> check_prime_numbers(vector<int> numbers){
+vector<int> check_prime_numbers(int max){
+  
   vector<int> prime_numbers;
-  for(int i = 0; i < numbers.size();i++){
+  vector<int> numbers_to_check;
+  
+  for(int i = 1; i <= max;i++){
+    numbers_to_check.push_back(i);
+  }
+  for(int i = 0; i < numbers_to_check.size();i++){
     int counter {0};
-    for(int j = 1; j <= numbers[i];j++){
-      if(numbers[i] % j == 0)
+    for(int j = 1; j <= numbers_to_check[i];j++){
+      if(numbers_to_check[i] % j == 0)
         counter++;
     }
     if(counter == 2)
-      prime_numbers.push_back(numbers[i]);
+      prime_numbers.push_back(numbers_to_check[i]);
   }
   return prime_numbers;
 }
 
 int main(){
-  vector<int> numbers_to_check;
-  for(int i = 1; i <= 100;i++){
-    numbers_to_check.push_back(i);
-  }
-  vector<int> my_prime = check_prime_numbers(numbers_to_check);
+
+  int max = 50;
+  vector<int> my_prime = check_prime_numbers(50);
   for(int i = 0; i < my_prime.size();i++)
     cout << my_prime[i] << "\n";
 }
