284e78293cbca9a9df993c800abed6c91b86e4e5
CodingWithLaura <laura.ganss@web.de>
2023-04-23 22:06:16 +0200

    Improve read-in logic

--- chapter_four_excercises/src/main.cpp
+++ chapter_four_excercises/src/main.cpp
@@ -28,14 +28,33 @@ int find_mode_value(vector<int> values){
   return mode;
 }
 
+bool is_number(string input_str) {
+  for (int i = 0;i<input_str.length();i++){
+    if ((input_str[i])-'0' < 0 || (input_str[i])-'0' > 9){
+      return false;
+    }
+  }
+  return true;
+}
 
 int main(){
   int x {0};
   int input {0};
+  string input_str{""};
   vector<int> numbers;
-  
-  while(x < 10) {
-    cin >> input;
+  bool run{true};
+  while(run) {
+    cin >> input_str;
+    //check input
+    if (!is_number(input_str) && input_str != "|") {
+      cout << "no number or | \n";
+      return -1;
+    }
+    if (input_str == "|") {
+      run = false;
+      break;
+    }
+    input = std::stoi(input_str);
     numbers.push_back(input);
     x++;
   }
