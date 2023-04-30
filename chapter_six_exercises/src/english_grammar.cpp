c37ce0e21b829d223697bf8cf0a53d4c4dd6ffcb
CodingWithLaura <laura.ganss@web.de>
2023-04-30 19:35:47 +0200

    Rewrite grammar from book to add articles to sentences

---
+++ chapter_six_exercises/src/english_grammar.cpp
@@ -0,0 +1,27 @@
+#include "std_lib_facilities.h"
+
+/* english grammar rules:
+
+   Sentence:
+       Article Noun Verb
+       Sentence Conjuction Sentence
+   Conjunction:
+       "and"
+       "or"
+       "but"
+   Noun:
+       "birds"
+       "fish"
+       "C++"
+   Article:
+       "the"
+   Verb:
+       "rules"
+       "fly"
+       "swim"
+*/
+       
+int main(){
+  cout << "Hello\n";
+  //write a program which checks if a setence has the correct grammar
+}
