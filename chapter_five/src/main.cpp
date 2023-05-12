c12e4535b06cb135ae053530a193128ca165450c
CodingWithLaura <laura.ganss@web.de>
2023-05-12 12:42:42 +0200

    Check in code with logic error to find out problem

--- chapter_five/src/main.cpp
+++ chapter_five/src/main.cpp
@@ -1,6 +1,23 @@
 #include "std_lib_facilities.h"
 
 int main(){
-  //25.
-  cout << "Success!\n";
+  vector<double> temps;
+
+  for(double temp; cin >> temp;)
+    temps.push_back(temp);
+
+  double sum = 0;
+  double high_temp = 0;
+  double low_temp = 0;
+
+  for(double x : temps){
+
+    if(x > high_temp) high_temp = x;
+    if(x < low_temp) low_temp = x;
+    sum += x;
+  }
+
+  cout << "High Temperature: " << high_temp << "\n";
+  cout << "Low temperature: " << low_temp << "\n";
+  cout << "Average temperature: " << sum/temps.size() << "\n";
 }
