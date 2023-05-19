a83bedaf596edcfbb8c98edf462bf77a465f1d97
toxypiks <toxypiks@noemail.net>
2023-05-19 19:11:23 +0200

    Add proper error handling to exercise

---
+++ chapter_five/src/main.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+int main()
+try {
+  cout << "Success!\n";
+  return 0;
+}
+catch(exception& e) {
+  cerr << "error: " << e.what() << '\n';
+  return 1;
+}
+catch(...) {
+  cerr << "Oops: unknown exception!\n";
+  return 2;
+}
