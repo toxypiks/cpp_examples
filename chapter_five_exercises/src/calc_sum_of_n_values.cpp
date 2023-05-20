92f83415b7a13e5d5e2257896b36103f1ddc92a7
toxypiks <HannaHinkelbein89@gmx.de>
2023-05-20 18:21:29 +0200

    Add error handling for input

--- chapter_five_exercises/src/calc_sum_of_n_values.cpp
+++ chapter_five_exercises/src/calc_sum_of_n_values.cpp
@@ -1,11 +1,19 @@
 #include "std_lib_facilities.h"
 
 int calc_sum_of_n_values(int n, vector<int> values) {
-  int sum {0};
-  for(int i = 0; i < n; i++) {
-    sum = sum + values[i];
+  if(n > values.size())
+    throw invalid_argument("n higher then given numbers of values");
+  else if(n <= 0)
+    throw invalid_argument("n cant be negative or zero");
+  else if(values.size() == 0)
+    throw invalid_argument("no values given");
+  else {
+    int sum {0};
+    for(int i = 0; i < n; i++) {
+      sum = sum + values[i];
+    }
+    return sum;
   }
-  return sum;
 }
 
 
@@ -13,12 +21,19 @@ int main(){
   int num_values {0};
   cout << "Please enter the number of values you want to sum: \n";
   cin >> num_values;
-  vector<int> values;
-  int value;
-  cout << "Please enter some integers (press '|' to stop): \n";
-  while(cin>>value)
-    values.push_back(value);
-  int result = calc_sum_of_n_values(num_values,values);
-  cout << "The sum of the first " << num_values << " is " << result << '\n';
-  
+  try {
+    vector<int> values;
+    int value;
+    cout << "Please enter some integers (press '|' to stop): \n";
+    while(cin>>value)
+      values.push_back(value);
+    int result = calc_sum_of_n_values(num_values,values);
+    cout << "The sum of the first " << num_values << " numbers (" << values[0] << ", " << values[1] << ", " << values[2] << ")" << " is " << result << ".\n";
+    return 0;
+  }
+  catch (invalid_argument& e)
+  {
+    cerr << e.what() << '\n';
+    return -1;
+  }
 }
