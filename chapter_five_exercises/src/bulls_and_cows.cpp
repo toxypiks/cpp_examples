b3dfa3259571197f9ba02cd1dab925c14743550b
toxypiks <HannaHinkelbein89@gmx.de>
2023-05-29 17:56:33 +0200

    Let game run till user guessed all numbers correctly

--- chapter_five_exercises/src/bulls_and_cows.cpp
+++ chapter_five_exercises/src/bulls_and_cows.cpp
@@ -37,13 +37,49 @@ vector<Guesses> compare_guess_with_actual (vector<int> rand_num, vector<int> use
   return results;
 }
 
+bool check_won(vector<Guesses> guesses) {
+  int cows {0};
+  int bulls {0};
+  for(int i = 0; i < guesses.size(); i++)
+	if(guesses[i] == Guesses::COW)
+	  cows += 1;
+	else if (guesses[i] == Guesses::BULL)
+	  bulls += 1;
+
+  if (bulls == 4) {
+	return true;
+  }
+  return false;
+}
+
+void format_output(vector<Guesses> guesses) {
+  int cows {0};
+  int bulls {0};
+  for(int i = 0; i < guesses.size(); i++)
+	if(guesses[i] == Guesses::COW)
+	  cows += 1;
+	else if (guesses[i] == Guesses::BULL)
+	  bulls += 1;
+  cout << "You have: " << cows << " cows and " << bulls << " bulls!\n";
+}
+
 int main() {
+  cout << "Lets play a guessing game. Guess four unique numbers between 1 - 10 and see if they are correct (right number & right place = bull, right number wrong place = cow): \n";
+  bool won = false;
   vector<int> rand_test = four_random_number_gen();
-  for (int rand : rand_test)
-	cout << rand << '\n';
-  cout << std::endl;
-  vector<Guesses> my_guesses = compare_guess_with_actual(rand_test, vector<int>({1,2,3,4}));
-  for (Guesses guess : my_guesses)
-	cout << guess << '\n';
-  return 0;  
+  while(!won) {
+    int input {0};
+    vector <int> user_inputs {};
+    for (int i = 0; i < 4; i++) {
+	  if(cin >> input)
+	    user_inputs.push_back(input);
+	  else throw invalid_argument("only integers as input are allowed");
+    }
+    cout << std::endl;
+    vector<Guesses> my_guesses = compare_guess_with_actual(rand_test, user_inputs);
+	
+    format_output(my_guesses);
+    won = check_won(my_guesses);
+  }
+  return 0;
 }
